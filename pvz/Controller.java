70ec9b3ad92072aca1530ae6208d4e8392b00828
stuartmac <s.macdonald@null.net>
2013-10-23 21:33:09 -0400

    sorted main in Controller. problem with non-static methods

--- pvz/Controller.java
+++ pvz/Controller.java
@@ -3,37 +3,37 @@ package pvz;
 import java.util.Scanner;
 
 public class Controller {
-	private String plantChoosen;
+	private String plantChosen;
 	private int row,col;
 	private Scanner scanInput;
 	private Scanner readInput;
-	private Model gameModel;
+	private static Model gameModel;
 	
-	public Tile setTile(String plantName)
-	{
+	public Tile getTile(){
 		System.out.println("Where do you want to place the plant. Example: 0 1,4 5 (Max rows up to 6, max column up to 12).");
-		gameModel=new Model(1);
+		//gameModel=new Model(1);
 		readInput=new Scanner(System.in);
 		scanInput=new Scanner(readInput.nextLine());
-
+		
 		if(scanInput.hasNext())
 		{
 			try
 			{
-			int rowInput=Integer.parseInt(scanInput.next());
+			row=Integer.parseInt(scanInput.next());
 			if(scanInput.hasNext())
 			{
-				int colInput=Integer.parseInt(scanInput.next());
+				col=Integer.parseInt(scanInput.next());
+				return gameModel.getTile(col, row);
 				
 			}
 			else
 			{
-				System.out.println("Invaild command: No value for column was entered.");
+				System.out.println("Invalid command: No value for column was entered.");
 			}
 			}
 			catch(Exception e)
 			{
-				System.out.println("Invaild value was entered.");
+				System.out.println("Invalid value was entered.");
 			}
 		
 		}
@@ -45,82 +45,65 @@ public class Controller {
 	}
 	
 	
-	public Tile getTile(int row, int col)
-	{
-		return null;
-	}
-	
-	public boolean setStringPlant()
-	{
-		  System.out.println("What plant would you like to buy? (Type 1 or 2) 1:Sunflower 2:Shooter");
-		     readInput=new Scanner(System.in);
-			 scanInput=new Scanner(readInput.nextLine());
-			if(scanInput.hasNext())
-			{
-				try
-				{
+	public String setStringPlant(){
+		System.out.println("What plant would you like to buy? (Type 1 or 2) 1:Sunflower 2:Shooter");
+		readInput=new Scanner(System.in);
+		scanInput=new Scanner(readInput.nextLine());
+		if(scanInput.hasNext()){
+			try{
 				int userInput=Integer.parseInt(scanInput.next());
 
-				if(userInput<0 && userInput>2)
-				{
+				if(userInput<0 && userInput>2){
 					System.out.println("Item does not exist.");
-					return false;
-					
+					return null;
 				}
 				else{
-					switch (userInput)
-					{
+					switch (userInput){
 					case 1:
-						plantChoosen="sunflower";
-						return true;
+						plantChosen="sunflower";
+						break;
 					case 2:
-						plantChoosen="shooter";
-						return true;
+						plantChosen="shooter";
+						break;
 					default:
-						plantChoosen="None";
-						return true;
+						plantChosen="";
+						break;
 				    }
-				 }
-				 }
-				catch(Exception e)
-				{
-					System.out.println("Invalid command: No integer value entered.");
-					return false;
+					return plantChosen;
 				}
-				
 			}
-			return false;
-	      }
+			catch(Exception e){
+				System.out.println("Invalid command: No integer value entered.");
+				return null;
+			}
+		}
+		return null;
+	}
 
-
-
-	public static void main(String arg[])
-	{
+	public static void main(String arg[]){
+		gameModel = new Model(1);
 		int gameState = 0;
-		//Model gameModel=new Model(1);
+		String newPlant = "";
+		Tile tempTile = null;
 		while(gameState == 0){
-
-		
-		/*//get player input
-			//possibly place a plant
-			update();
-			gameState = winState();
+			newPlant = setStringPlant();
+			if(newPlant != ""){
+				tempTile = getTile();
+				if(tempTile != null){
+					gameModel.placePlant(tempTile, newPlant);
+				}
+			}
+			gameModel.update();
+			gameState = gameModel.state();
 		}
 		if(gameState == 1){
-			//You won! Congraturation
+			System.out.println("You won. You killed all the zombies.");
 		}
 		else if(gameState == -1){
-			//You lost. Mission Failed
+			System.out.println("You lost. The zombies ate your brains.");
 		}
-		return 0;
 		
 	}
-	*/
 
-        }
-    }
-
-
-	
 }
 
